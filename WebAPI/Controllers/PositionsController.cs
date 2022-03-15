e2e162196e27e41a5f09dcbbf3f3f3926c0f2833
Claudio Camargos <claudiocamargos@gmail.com>
2022-03-15 12:03:51 -0300

    Conclusão Lista posições

--- WebAPI/Controllers/PositionsController.cs
+++ WebAPI/Controllers/PositionsController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Positions.Queries.QueryTopPositions;
+﻿using Application.Positions.Queries.QueryAllPositions;
+using Application.Positions.Queries.QueryTopPositions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,8 +8,14 @@ namespace WebAPI.Controllers
     [Authorize]
     public class PositionsController : ApiControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<PositionDto>>> GetAll()
+        {
+            return await Mediator.Send(new GetAllPositionsQuery());
+        }
+
         [HttpGet("Tops7Days")]
-        public async Task<ActionResult<List<TopPositionDto>>> Get([FromQuery] GetTop7DaysPositionsQuery query)
+        public async Task<ActionResult<List<TopPositionDto>>> GetTops([FromQuery] GetTop7DaysPositionsQuery query)
         {
             return await Mediator.Send(query);
         }
