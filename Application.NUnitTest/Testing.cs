8b8fa0dda27b680216049c2659eb35c8a5636e75
Claudio Camargos <claudiocamargos@gmail.com>
2022-03-15 15:24:51 -0300

    Testes

---
+++ Application.NUnitTest/Testing.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Moq;
+using Application.Commom.Interfaces;
+using WebAPI.Services;
+using WebAPI.Filters;
+using FluentValidation.AspNetCore;
+using Infrastructure.Persitence;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using MediatR;
+using NUnit.Framework;
+using Infrastructure;
+
+namespace Application.Test
+{
+    [SetUpFixture]
+    public class Testing
+    {
+        private static IConfigurationRoot _configuration = null!;
+        private static IServiceScopeFactory _scopeFactory = null!;
+        private static int _currentUserId = 1;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+                w.EnvironmentName == "Development" &&
+                w.ApplicationName == "WebAPI"));
+
+            services.AddLogging();
+            
+            services.AddApplication();
+            services.AddInfrastructure(_configuration);
+            services.AddSingleton<ICurrentUserService, CurrentUserService>();
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddHttpContextAccessor();
+            services.AddHealthChecks();
+            services.AddControllersWithViews(options =>
+                options.Filters.Add<ApiExceptionFilterAttribute>())
+                    .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+
+            services.AddTransient(provider =>
+                Mock.Of<ICurrentUserService>(s => s.UserId == _currentUserId));
+
+            _scopeFactory = services.BuildServiceProvider().GetRequiredService<IServiceScopeFactory>();
+
+            ValidateDatabase();
+        }
+
+        private static void ValidateDatabase()
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ToroDbContext>();
+            context.Database.Migrate();
+        }
+
+        public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+            return await mediator.Send(request);
+        }
+    }
+}
